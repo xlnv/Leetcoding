afccb86834ecfa7adbcee6f6e8493119afa54e94
admin <1907359500@qq.com>
2025-05-27 11:32:25 +0800

    第15题. 三数之和

---
+++ src/HashTable/Solution08.java
@@ -0,0 +1,60 @@
+package HashTable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class Solution08 {
+    public static void main(String[] args) {
+        //给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有满足条件且不重复的三元组。
+        //
+        //注意： 答案中不可以包含重复的三元组。
+        //
+        //示例：
+        //
+        //给定数组 nums = [-1, 0, 1, 2, -1, -4]，
+        //
+        //满足要求的三元组集合为： [ [-1, 0, 1], [-1, -1, 2] ]
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        List<List<Integer>> res = threeSum(nums);
+        System.out.println(res);
+    }
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        // 找出a + b + c = 0
+        // a = nums[i], b = nums[left], c = nums[right]
+        for (int i = 0; i < nums.length; i++) {
+            // 排序之后如果第一个元素已经大于零，那么无论如何组合都不可能凑成三元组，直接返回结果就可以了
+            if (nums[i] > 0) {
+                return result;
+            }
+
+            if (i > 0 && nums[i] == nums[i - 1]) {  // 去重a
+                continue;
+            }
+
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (right > left) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum > 0) {
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    // 去重逻辑应该放在找到一个三元组之后，对b 和 c去重
+                    while (right > left && nums[right] == nums[right - 1]) right--;
+                    while (right > left && nums[left] == nums[left + 1]) left++;
+
+                    right--;
+                    left++;
+                }
+            }
+        }
+        return result;
+    }
+}
