ac483b1b375c2dbe93b4cb0d664a7177afa46fe5
admin <1907359500@qq.com>
2025-05-28 20:04:47 +0800

    35.搜索插入的位置

--- src/Array/Solution02.java
+++ src/Array/Solution02.java
@@ -3,9 +3,7 @@ package Array;
 public class Solution02 {
 
     public static void main(String[] args) {
-        //给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
-        //
-        //如果数组中不存在目标值 target，返回 [-1, -1]。
+        //给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
         int[] nums ={1,3,5,6};
         int target = 7;
         int result = search(nums, target);
