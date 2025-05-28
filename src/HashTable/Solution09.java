12ffdaf506e7aa52e81e220f4bc53fb3ac4d5e19
admin <1907359500@qq.com>
2025-05-28 09:32:40 +0800

    18. 四数之和

--- src/HashTable/Solution09.java
+++ src/HashTable/Solution09.java
@@ -18,41 +18,62 @@ public class Solution09 {
         List<List<Integer>> res = fourSum(nums, target);
         System.out.println(res);
 
+
     }
 
     public static List<List<Integer>> fourSum(int[] nums, int target) {
         List<List<Integer>> result = new ArrayList<>();
+        // 如果数组长度小于4，直接返回空结果
         if (nums == null || nums.length < 4) {
             return result;
         }
+
+        // 先对数组进行排序，方便后续处理重复元素和双指针移动
         Arrays.sort(nums);
         int n = nums.length;
 
+        // 外层循环固定第一个数
         for (int i = 0; i < n - 3; i++) {
+            // 跳过重复的第一个数
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
+
+            // 内层循环固定第二个数
             for (int j = i + 1; j < n - 2; j++) {
+                // 跳过重复的第二个数
                 if (j > i + 1 && nums[j] == nums[j - 1]) {
                     continue;
                 }
+
+                // 使用双指针寻找剩下的两个数
                 int left = j + 1;
                 int right = n - 1;
+
                 while (left < right) {
+                    // 计算当前四个数的和
                     int sum = nums[i] + nums[j] + nums[left] + nums[right];
+
                     if (sum == target) {
+                        // 找到符合条件的四元组，加入结果列表
                         result.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+
+                        // 跳过重复的第三个数
                         while (left < right && nums[left] == nums[left + 1]) {
                             left++;
                         }
+                        // 跳过重复的第四个数
                         while (left < right && nums[right] == nums[right - 1]) {
                             right--;
                         }
+                        // 移动指针寻找下一组可能的解
                         left++;
                         right--;
                     } else if (sum < target) {
+                        // 和太小，左指针右移
                         left++;
-                    }else {
+                    } else {
+                        // 和太大，右指针左移
                         right--;
                     }
                 }
