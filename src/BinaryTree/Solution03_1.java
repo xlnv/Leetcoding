5436110313fce01b244af3f625ef1155068329e8
admin <1907359500@qq.com>
2025-05-29 20:12:16 +0800

    144.二叉树的前序遍历迭代法

--- src/BinaryTree/Solution03_1.java
+++ src/BinaryTree/Solution03_1.java
@@ -1,6 +1,7 @@
 package BinaryTree;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Stack;
 
@@ -21,24 +22,23 @@ public class Solution03_1 {
     }
 
     public static List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> result = new ArrayList<>();
-        Stack<TreeNode> st = new Stack<>(); // 模拟递归栈
-        if(root != null)st.push(root); // 根节点入栈
-
+        List<Integer> result = new LinkedList<>();
+        Stack<TreeNode> st = new Stack<>();
+        if (root != null) st.push(root);
         while (!st.empty()) {
-            TreeNode node = st.peek(); // 查看栈顶元素
+            TreeNode node = st.peek();
             if (node != null) {
+                st.pop(); // 将该节点弹出，避免重复操作，下面再将右左中节点添加到栈中（前序遍历-中左右，入栈顺序右左中）
+                if (node.right!=null) st.push(node.right);  // 添加右节点（空节点不入栈）
+                if (node.left!=null) st.push(node.left);    // 添加左节点（空节点不入栈）
+                st.push(node);                          // 添加中节点
+                st.push(null); // 中节点访问过，但是还没有处理，加入空节点做为标记。
+
+            } else { // 只有遇到空节点的时候，才将下一个节点放进结果集
+                st.pop();           // 将空节点弹出
+                node = st.peek();    // 重新取出栈中元素
                 st.pop();
-                // 前序顺序：中 → 左 → 右，入栈顺序逆序：右 → 左 → 中
-                if (node.right != null) st.push(node.right);
-                if (node.left != null) st.push(node.left);
-                st.push(node);
-                st.push(null);
-            } else {
-                st.pop(); // 弹出null
-                node = st.peek();
-                st.pop();
-                result.add(node.val);
+                result.add(node.val); // 加入到结果集
             }
         }
         return result;
