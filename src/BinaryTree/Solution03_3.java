de044839a404690be016bc66c228bb84fe856755
admin <1907359500@qq.com>
2025-05-29 17:28:47 +0800

    145.二叉树的后序遍历迭代法

---
+++ src/BinaryTree/Solution03_3.java
@@ -0,0 +1,50 @@
+package BinaryTree;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+public class Solution03_3 {
+    public static void main(String[] args) {
+        // 树结构：
+        //        1
+        //       / \
+        //      2   3
+        //     / \ / \
+        //    4  5 6  7
+
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(7);
+
+        List<Integer> res = postorderTraversal(root);
+        System.out.println(res);//[4,5,2,6,7,3,1]
+
+    }
+    public static List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new LinkedList<>();
+        Stack<TreeNode> st = new Stack<>();
+        if (root != null) st.push(root);
+        while (!st.empty()) {
+            TreeNode node = st.peek();
+            if (node != null) {
+                st.pop(); // 将该节点弹出，避免重复操作，下面再将中右左节点添加到栈中（后序遍历-左右中，入栈顺序中右左）
+                st.push(node);                          // 添加中节点
+                st.push(null); // 中节点访问过，但是还没有处理，加入空节点做为标记。
+                if (node.right!=null) st.push(node.right);  // 添加右节点（空节点不入栈）
+                if (node.left!=null) st.push(node.left);    // 添加左节点（空节点不入栈）
+
+            } else { // 只有遇到空节点的时候，才将下一个节点放进结果集
+                st.pop();           // 将空节点弹出
+                node = st.peek();    // 重新取出栈中元素
+                st.pop();
+                result.add(node.val); // 加入到结果集
+            }
+        }
+        return result;
+    }
+}
