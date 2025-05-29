a32295892ffc56285b208fee8a8aa86be21e1245
admin <1907359500@qq.com>
2025-05-29 15:13:38 +0800

    144.二叉树的前序遍历

--- src/BinaryTree/Solution02_1.java
+++ src/BinaryTree/Solution02_1.java
@@ -11,12 +11,12 @@ public class Solution02_1 {
         //       / \
         //      2   3
         //     / \ / \
-        //  null  5 6 7
+        //     4  5 6 7
 
         TreeNode root = new TreeNode(1);
         root.left = new TreeNode(2);
         root.right = new TreeNode(3);
-        root.left.left = null;
+        root.left.left = new TreeNode(4);
         root.left.right = new TreeNode(5);
         root.right.left = new TreeNode(6);
         root.right.right = new TreeNode(7);
