7f334dc6e5b09643e5d478409427ddb1bf4bffc5
admin <1907359500@qq.com>
2025-05-30 19:04:14 +0800

    102.二叉树的层序遍历

--- src/BinaryTree/Solution05_1.java
+++ src/BinaryTree/Solution05_1.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
-public class Solution05 {
+public class Solution05_1 {
     public static void main(String[] args) {
         //102.二叉树的层序遍历
         //给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
