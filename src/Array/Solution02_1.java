bcdfd7fb208018129fce283d37e71d718682f816
admin <1907359500@qq.com>
2025-06-19 08:24:28 +0800

    34. 在排序数组中查找元素的第一个和最后一个位置

--- src/Array/Solution02_1.java
+++ src/Array/Solution02_1.java
@@ -2,6 +2,7 @@ package Array;
 
 public class Solution02_1 {
     public static void main(String[] args) {
+        //34. 在排序数组中查找元素的第一个和最后一个位置
         //给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
         //
         //如果数组中不存在目标值 target，返回 [-1, -1]。
