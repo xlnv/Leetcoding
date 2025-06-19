38a5bfb70b587afc693f6f413971e4052560f08c
admin <1907359500@qq.com>
2025-06-19 08:21:57 +0800

    704. 二分查找

--- src/Array/Solution01.java
+++ src/Array/Solution01.java
@@ -3,9 +3,10 @@ package Array;
 public class Solution01 {
 
     public static void main(String[] args) {
+        //704. 二分查找
         //给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target
-        // ，写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
-        //关键词：有序数组、无重复元素
+        //        // ，写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
+        //        //关键词：有序数组、无重复元素
         int[] num1 = {-1,0,3,5,9,12};
         int target1 = 9;
         int result = search2(num1, target1);
