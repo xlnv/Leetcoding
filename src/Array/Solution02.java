f10b60df35146e8bc633881fe70cd97b6bb1a4da
admin <1907359500@qq.com>
2025-06-19 08:23:49 +0800

    35.搜索插入位置

--- src/Array/Solution02.java
+++ src/Array/Solution02.java
@@ -3,6 +3,7 @@ package Array;
 public class Solution02 {
 
     public static void main(String[] args) {
+        //35.搜索插入位置
         //给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
         int[] nums ={1,3,5,6};
         int target = 7;
